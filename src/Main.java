4b99f6cf5d6e89052958b7a82a25e868b461aafc
volynkina <bzzz18@yandex.ru>
2023-01-25 19:55:11 +0500

    task2

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        String firstName = "Ivan";
-        String middleName = "Ivanovich";
-        String lastName = "Ivanov";
+        String firstName = "Ivan ";
+        String middleName = "Ivanovich ";
+        String lastName = "Ivanov ";
         String fullName = lastName + firstName+middleName;
         System.out.println("Ф.И.О. сотрудника - " + fullName);
         System.out.println();
-
+        String fullNameBig = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullNameBig);
     }
 }
